c89fcbe3abc696ea285236c94a205e7ce17afd7b
uknowwho <florianvandersteen@gmail.com>
2024-05-03 11:45:43 +0200

    05: opdr 5.0, issue 15; werkende ShopDoc zonder frontend verbeteringen

--- views/ShopDoc.php
+++ views/ShopDoc.php
@@ -12,6 +12,13 @@ class ShopDoc extends ProductDoc {
             echo '<p>Prijs: &euro;' . $product->price / 100  . ',-</p>';
             echo '</div></a>' . PHP_EOL;
             
+            // code for ratings
+            echo '<div class="ratingStars">';
+            for ($i = 1; $i <= 5; $i++) {
+                echo '<span id="p' . $product->id .'-star-' . $i . '" class="fa">&#xf006;</span>'; 
+            }
+            echo '</div>';
+
             if ($this->model->isUserLoggedIn()) {
                 $this->showActionButton("addToCart", "shop", "cartAddButton", "Voeg toe aan CART", $product->id);
             }
@@ -19,6 +26,74 @@ class ShopDoc extends ProductDoc {
         }
 
         echo '</div>';
-        
+    }
+
+    protected function showScripts() {
+        echo '<script language="JavaScript" type="text/javascript" src="/../jquery-3.7.1.min.js"></script>';
+        $products = $this->model->products;
+        echo '<script>
+        $(document).ready(function() {';
+            foreach($products as $product) {
+                    echo '$.get("index.php?action=ajax&function=getAvgRating&id=' .  $product->id . '", function(json) {
+                        var data = JSON.parse(json);
+                        var avgRating = Math.round(data.avg_rating * 2) / 2;
+                        showStars(avgRating, ' . $product->id . ');
+                    })
+                    .fail(function() {
+                        // Error callback function
+                        console.error("Error occurred while sending the request.");
+                    });';
+                    echo 'for (var i = 1; i <= 5; i++) {
+                        (function(index) {
+                            $("#p' . $product->id . '-star-" + index).click(function() {
+                                changeStars(index, ' . $product->id .')
+                            })
+                        })(i)
+                    }';
+            }
+            
+            echo 'function showStars(avgRating, productId) {
+                for (var i = 1; i <= 5; i++) {
+                    var diff = avgRating - i
+                    // half a star
+                    if (diff == 0.5) {
+                        $("#p" + productId + "-star-" + i).addClass("fa").html("&#xf123;")
+                    } 
+                    // full star
+                    else if (diff == 0 || diff >= 1) {
+                        $("#p" + productId + "-star-" + i).addClass("fa").html("&#xf005;")
+                    }
+                    // empty star 
+                    else {
+                        $("#p" + productId + "-star-" + i).addClass("fa").html("&#xf006;")
+                    }
+                }
+            }
+                    
+            function changeStars(number, productId) {
+                $.get("index.php?action=ajax&function=updateRating&rating=" + number + "&id=" + productId, function(json) {})
+                .fail(function() {
+                    // Error callback function
+                    console.error("Error occurred while sending the request.")
+                })
+                $.get("index.php?action=ajax&function=getAvgRating&id=" + productId, function(json) {
+                    const data = JSON.parse(json)
+                    var avgRating = Math.round(data.avg_rating*2)/2
+                    showStars(avgRating, productId)
+                })
+                .fail(function() {
+                    // Error callback function
+                    console.error("Error occurred while sending the request.")
+                })
+            }
+        })
+        </script>';
+    }
+
+    protected function showStyleLinks() {
+        // dit werkt nu niet meer als ik het test vanuit tests/
+        echo '<link rel="stylesheet" type="text/css" href="css/styles.css">';
+        echo '<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Merriweather|Open+Sans">';
+        echo '<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">';
     }
 }
